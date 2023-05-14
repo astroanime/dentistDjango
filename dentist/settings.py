87bf4486a38786d8800865056ff0e9c9326ee505
Abeuov Olzhas <abeuov.o0705@gmail.com>
2023-05-14 23:29:29 +0600

    'heroku_again'

--- dentist/settings.py
+++ dentist/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'ik8#s8=-xx#a1(0lzyp1h3liu3g-roe_0cld+*j9krwg2)@=xu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
